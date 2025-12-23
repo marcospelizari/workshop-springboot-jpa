1346d6e99265155d053ecdb83aa1e417adeeb4c2
Marcos <132465567+marcospelizari@users.noreply.github.com>
2025-12-23 10:01:52 -0300

    Products entity, service, controller e repository

---
+++ src/main/java/com/educandoweb/course/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.service;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = productRepository.findById(id);
+        return obj.get();
+    }
+}
