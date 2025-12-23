c350b538024d7af1cf5950581f93df237dda7746
Marcos <132465567+marcospelizari@users.noreply.github.com>
2025-12-23 13:23:41 -0300

    User update

--- src/main/java/com/educandoweb/course/controller/UserController.java
+++ src/main/java/com/educandoweb/course/controller/UserController.java
@@ -44,4 +44,10 @@ public class UserController {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj ) {
+        obj = userService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
